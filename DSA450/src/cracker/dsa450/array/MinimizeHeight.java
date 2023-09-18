0180a04315c53732aaa203ff9db7cf1c831ea8a3
Parth Solanki <parthsolanki1498@gmail.com>
2023-09-18 17:39:08 -0400

    MinimizeHeights

--- DSA450/src/cracker/dsa450/array/MinimizeHeight.java
+++ DSA450/src/cracker/dsa450/array/MinimizeHeight.java
@@ -6,14 +6,38 @@ public class MinimizeHeight {
 	
 	public static void main(String[] args) {
 		
-		//int array[] = {1,5,8,10};
-		int array[] = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
+		// int array[] = {1,5,8,10};
+		// int array[] = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
+		int array[] = {7,4,8,8,8,9};
 		//int array[] = {3,9,12,16,20};
-		int k = 5;
+		int k = 6;
 		Arrays.sort(array);
-		minimizeHeight(array,k);
-		
-		
+		// minimizeHeight(array,k);
+		minimizeHeightAccepted(array, k);	
+	}
+
+	private static void minimizeHeightAccepted(int[] array, int k) {
+		Arrays.sort(array);
+		int n = array.length;
+
+		// Maximum possible height difference
+		int ans = array[n-1] - array[0];
+
+		int tempmin, tempmax;
+		tempmin = array[0];
+		tempmax = array[n-1];
+
+		for(int i = 1; i < n; i++){
+			if(array[i] - k < 0){
+				continue;
+			}
+
+			tempmin = Math.min(array[0] + k, array[i] - k);
+			tempmax = Math.max(array[i-1] + k, array[n - 1] - k);
+
+			ans = Math.min(ans,tempmax - tempmin);
+		}
+		System.out.println(ans);
 	}
 
 	private static void minimizeHeight(int[] array, int k) {
@@ -24,14 +48,17 @@ public class MinimizeHeight {
 			} else {
 				array[i] = array[i] - k;
 			}
+
+			System.out.println("loop ele - " + array[i]);
 		}
-		//Arrays.sort(array);
+		System.out.println("=====");
+		Arrays.sort(array);
 		for(int ele : array) {
 			System.out.println(ele);
 		}
 		
 		
-		//System.out.println(array[array.length - 1] - array[0]);
+		System.out.println("Ans " + (array[array.length - 1] - array[0]));
 	}
 
 }
